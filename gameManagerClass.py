ccd964a1ba871f78f94c03a5eaa6a319a9ddf744
Georgiana <200639122+Georgiana2112@users.noreply.github.com>
2025-12-22 14:35:20 +0200

    logica inceput

---
+++ gameManagerClass.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+
+
+
+class GameManager:
+    PLAYER=1
+    COMPUTER = 2
+    def __init__(self, buttons):
+        self.turn = self.PLAYER
+        self.buttons = buttons # butoanele din interfata
+        self.game_matrix = [[0 for _ in range(3)] for _ in range(3)] # matrice de 3 x 3 cu zerouri
+
+    def game_ended(self):
+        #cazuri in care castiga cineva
+        for i in range(0,3):  # verific liniile daca is de acelas tip mergand pe centru 01 11 21 cu vecinii stanga dreapta
+            if(self.game_matrix[i][1] == self.PLAYER or self.game_matrix[i][1]==self.COMPUTER):
+                if self.game_matrix[i][1] == self.game_matrix[i][0] and self.game_matrix[i][1] == self.game_matrix[i][2]:
+                    return self.PLAYER if self.turn == self.COMPUTER else self.COMPUTER
+        for i in range(0, 3):  # verific coloanele, verific [1][0], [1][1], [1][2] si vecinii
+            if (self.game_matrix[1][i] == self.PLAYER or self.game_matrix[1][i] == self.COMPUTER):
+                if self.game_matrix[1][i] == self.game_matrix[0][i] and self.game_matrix[1][i] == self.game_matrix[2][i]:
+                    return self.PLAYER if self.turn == self.COMPUTER else self.COMPUTER
+
+        # verific diagonalele:
+        if (self.game_matrix[1][1] == self.PLAYER or self.game_matrix[1][1] == self.COMPUTER):
+            if self.game_matrix[0][0] == self.game_matrix[1][1] and self.game_matrix[1][1] == self.game_matrix[2][2]:
+                return self.PLAYER if self.turn == self.COMPUTER else self.COMPUTER
+            if self.game_matrix[1][1] == self.game_matrix[0][2] and self.game_matrix[0][1] == self.game_matrix[2][0]:
+                return self.PLAYER if self.turn == self.COMPUTER else self.COMPUTER
+
+        #daca jocul nu s-a terminat
+        for i in range(0,3):
+            for j in range(0, 3):
+                if self.game_matrix[i][j] != self.COMPUTER and self.game_matrix[i][j] != self.PLAYER:
+                    return 0
+
+        #daca jocul s-a terminat cu toate locurile ocupate
+        return 3
+
+    def verif_move(self,x,y):
+        if self.game_matrix[x][y] == 0:
+            return True #mutare valida
+        else:
+            return False
+
+    def buttonPressed(self, x, y):
+        if self.turn == GameManager.COMPUTER:
+            if self.verif_move(x,y):
+                self.buttons[x][y].configure(text="O")
+                self.turn = self.PLAYER
+                self.game_matrix[x][y] = GameManager.PLAYER
+
+        else:
+            if self.verif_move(x, y):
+                self.buttons[x][y].configure(text="X")
+                self.turn = GameManager.COMPUTER
+                self.game_matrix[x][y] = GameManager.COMPUTER
+
+    def reset(self):
+        for x in range(3):  # resetarea textului de pe butoane
+            for y in range(3):
+                self.game_matrix[x][y] = 0
--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import tkinter as tk
 from tkinter import messagebox, ttk, Label
 import tkinter.font as tkFont
 
-from PIL.ImageChops import offset
-from fontTools.misc.py23 import xrange
 
-from util import GameManager
+from gameManagerClass import GameManager
 
 
 #paleta de culori folosita: https://colorhunt.co/palette/97a87aa8bba3fcf9eaffa239
@@ -15,10 +13,16 @@ class TicTacToe:
         self.root = root
         self.root.title("TIC TAC TOE")
         self.root.geometry("890x653")
+        self.root.resizable(False, False)
         self.btn_x_0 = 0
         self.create_widgets()
-
         self.game_manager = GameManager(self.btn_x_0)
+
+    def on_btn_press(self, x, y):
+        self.game_manager.buttonPressed( x, y)
+        winner = self.game_manager.game_ended()
+        if (winner != 0):
+            self.show_game_over_window(winner) #1 - PLAYER, 2 - COMPUTER, 3 - EGALITATE
 
     def create_widgets(self):
         self.root.configure(background="#A8BBA3")
@@ -30,7 +34,7 @@ class TicTacToe:
             highlightthickness = 2,
             highlightbackground = "#97A87A"
         )
-        frame.place(x=100,y=10)
+        frame.place(x=100,y=30)
 
         #textul de sus
         font = tkFont.Font(family="Times New Roman", size = 30, weight = "bold")
@@ -44,12 +48,14 @@ class TicTacToe:
                 self.btn_x_0[x][y] = tk.Button(frame) #parinte -> frame
 
                 self.btn_x_0[x][y].config(
-                    height = 10,
-                    width = 20,
+                    font=("Helvetica", 36,"bold"),
+                    width=5,
+                    height=2,
                     bg = '#F7B980',
+                    fg = '#FCF9EA',
                     activebackground="#ED985F",
                     relief = tk.FLAT,
-                    command = lambda a=x, b=y: self.game_manager.buttonPressed( a, b)
+                    command = lambda a=x, b=y: self.on_btn_press( a, b)
                 ) #10 si 20 pentru a fi cat de cat patrate -> se masoara in unitate de text
                 self.btn_x_0[x][y].grid(row = x+1, column = y)
 
@@ -68,6 +74,49 @@ class TicTacToe:
                 if(x==2):
                     offset_jos = 20
                 self.btn_x_0[x][y].grid_configure(padx=(offset_stanga,offset_dreapta), pady=(offset_sus,offset_jos))
+
+    def show_game_over_window(self, winner):
+        win = tk.Toplevel(self.root)
+        win.title("")
+        win.geometry("300x180")
+        win.configure(bg="#FCF9EA")
+        win.resizable(False, False)
+
+
+        if winner == 1: # a castigat jucatorul
+            msg = "PLAYER WON"
+        elif winner == 2: # a castigat monte carlo
+            msg = "COMPUTER WON"
+        elif winner == 3: # egal
+            msg = "It's a tie!"
+
+        Font = tkFont.Font(family="Helvetica", size = 20, weight="bold")
+        label = tk.Label(win, text=msg,  bg="#FCF9EA",fg="#97A87A",font=Font)
+        label.pack(pady = (30,5))
+
+        # btn pentru joc nou
+        btn_restart = tk.Button(win, text="New Game", width=10, command=lambda: self.restart_game(win))
+        btn_restart.pack(side=tk.LEFT, padx=20, pady=10)
+        btn_restart.config( bg = '#F7B980',
+                            fg = 'black',
+                            activebackground="#ED985F",
+                            relief=tk.FLAT,
+                            )
+        # quit
+        btn_quit = tk.Button(win, text="Quit", width=10, command=self.root.destroy)
+        btn_quit.pack(side=tk.RIGHT, padx=20, pady=10)
+        btn_quit.config(bg='#F7B980',
+                       fg='black',
+                       activebackground="#ED985F",
+                       relief=tk.FLAT,
+                       )
+    def restart_game(self, window):
+        window.destroy()  # inchiderea ferestrei cu mesaj de final
+        for x in range(3):# resetarea textului de pe butoane
+            for y in range(3):
+                self.btn_x_0[x][y].configure(text="")
+
+        self.game_manager.reset()
 
 
 if __name__ == "__main__":
